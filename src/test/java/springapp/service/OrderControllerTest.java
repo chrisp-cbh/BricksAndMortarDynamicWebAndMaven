d960cd829446f00c8e105bc0c95fc4cb2db68b9d
afowl <alistir.fowler@travisperkins.co.uk>
2013-04-24 16:21:41 +0100

    Update Qty <--just to p*** alan off

--- src/test/java/springapp/service/OrderControllerTest.java
+++ src/test/java/springapp/service/OrderControllerTest.java
@@ -97,6 +97,7 @@ public class OrderControllerTest {
 	{
 		Calendar calendar = Calendar.getInstance();
 		calendar.set(year, month, day, 0,0,0);
+		calendar.set(Calendar.MILLISECOND, 0);
 		return calendar.getTime();
 	}
 }
