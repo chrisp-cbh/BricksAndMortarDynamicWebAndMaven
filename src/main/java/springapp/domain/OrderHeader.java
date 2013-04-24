33417f02ee97c3fe137fed76fbc971fe85d9aea3
noran <noreen.rank@travisperkins.co.uk>
2013-04-24 12:21:39 +0100

    Created by JASON AND NOREEN!!!!!!!!

---
+++ src/main/java/springapp/domain/OrderHeader.java
@@ -0,0 +1,16 @@
+package springapp.domain;
+
+import java.util.Date;
+
+public class OrderHeader {
+
+	public Date date;
+	public int userId;
+	public int tillId;
+	public int orderId;
+		
+	public OrderHeader()
+	{
+	
+	}
+}
