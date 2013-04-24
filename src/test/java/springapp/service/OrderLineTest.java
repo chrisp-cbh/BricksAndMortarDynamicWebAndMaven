46603c0ee1f5e5ce4d4d4ae52a6c69dedc5f2726
afowl <alistir.fowler@travisperkins.co.uk>
2013-04-24 11:50:27 +0100

    Initial Create

---
+++ src/test/java/springapp/service/OrderLineTest.java
@@ -0,0 +1,14 @@
+package springapp.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class OrderLineTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
