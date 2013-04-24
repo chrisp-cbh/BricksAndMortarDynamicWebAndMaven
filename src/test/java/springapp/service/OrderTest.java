0d18640386f8bc20f633d6aeac896073b9d77e4a
afowl <alistir.fowler@travisperkins.co.uk>
2013-04-24 12:32:12 +0100

    Updated Order class -- Changed field name.

--- src/test/java/springapp/service/OrderTest.java
+++ src/test/java/springapp/service/OrderTest.java
@@ -11,63 +11,34 @@ import java.util.Date;
 public class OrderTest {
 
 	@Test
-	public void checkTillIdIsReturnedCorrectly() {
-		Order order = new Order(1, 1, new Date());
-		assertEquals(1, order.tillId);
-	}
-
-	@Test
-	public void checkAnotherTillIdIsReturnedCorrectly() {
-		Order order = new Order(5, 1, new Date());
-		assertEquals(5, order.tillId);
-	}
-
-	@Test
-	public void checkSalesAssistentIdIsReturnedCorrectly() {
-		Order order = new Order(5, 1, new Date());
-		assertEquals(1, order.salesAssistentId);
-	}
-
-	@Test
-	public void checkAnotherSalesAssistentIdIsReturnedCorrectly() {
-		Order order = new Order(5, 2, new Date());
-		assertEquals(2, order.salesAssistentId);
+	public void orderObjectCanBeInstantiatedAndValuesAreSetCorrectly()
+	{
+		int tillId = 1;
+		int salesAssistantId = 12;
+		Date orderDate = new Date();
+		
+		Order order = new Order(tillId, salesAssistantId, orderDate);
+		
+		assertEquals(tillId, order.tillId);
+		assertEquals(salesAssistantId, order.salesAssistantId);
+		assertEquals(orderDate, order.date);
 	}
 	
 	@Test
-	public void checkDateIsReturnedCorrectly() {
-		Date dateToCheck = new Date();
+	public void orderObjectCanBeInstantiatedAndOtherValuesAreSetCorrectly()
+	{
+		int tillId = 2;
+		int salesAssistantId = 48;
 		
-		Order order = new Order(5, 1, dateToCheck);
-		assertEquals(dateToCheck, order.date);
-	}
-
-	@Test
-	public void checkAnotherDateIsReturnedCorrectly() {
 		Calendar calendar = Calendar.getInstance();
-		calendar.add(Calendar.SECOND, 1);
+		calendar.add(Calendar.MINUTE, 1);
 		
-		Date dateToCheck = calendar.getTime();
+		Date orderDate = calendar.getTime();
 		
-		Order order = new Order(5, 2, dateToCheck);
-		assertEquals(dateToCheck, order.date);
+		Order order = new Order(tillId, salesAssistantId, orderDate);
+		
+		assertEquals(tillId, order.tillId);
+		assertEquals(salesAssistantId, order.salesAssistantId);
+		assertEquals(orderDate, order.date);
 	}
-	
-	
-//	@Test
-//	public void checkOrderIDIsReturnedCorrectly() {
-//		Order order = new Order(5, 1);
-//		assertEquals(1, order.orderID);
-//	}
-//
-//	@Test
-//	public void checkAnotherOrderIDIsReturnedCorrectly() {
-//		Order order = new Order(5, 2);
-//		assertEquals(2, order.orderID);
-//	}
-
-	
-	
-	
-	
 }
