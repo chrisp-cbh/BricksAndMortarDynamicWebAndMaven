eff8220246ad1caa54691b368b77c200317cd066
mstar <mstar@s164191.tpplc.local>
2013-04-25 15:54:51 +0100

    Rocketship now has engines

--- src/springdemo/main/java/uk/co/tpplc/springexamples/rocketship/mstar/RocketShip.java
+++ src/springdemo/main/java/uk/co/tpplc/springexamples/rocketship/mstar/RocketShip.java
@@ -3,11 +3,24 @@ package uk.co.tpplc.springexamples.rocketship.mstar;
 public class RocketShip {
 
 	final String shipName;
+	protected Engine engine;
 	
 	public RocketShip(String name)
 	{
 		this.shipName = name;
 	}
+	
+	public void setEngine(Engine engine)
+	{
+		this.engine = engine;
+	}
+	
+	public String getEngineType()
+	{
+		return engine.type;
+	}
+	
+	
 	
 
 }
