43c94d126ada47aa02bd5e1e835298640fd153f8
epope <edward.pope@travisperkins.co.uk>
2013-04-26 09:42:28 +0100

    -

--- src/springdemo/test/java/uk/co/tpplc/springexample/rocketship/partha/EngineTest.java
+++ src/springdemo/test/java/uk/co/tpplc/springexample/rocketship/partha/EngineTest.java
@@ -1,28 +1,29 @@
 package uk.co.tpplc.springexample.rocketship.partha;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
+
+import uk.co.tpplc.springexamples.rocketship.partha2.ArianRocket;
+import uk.co.tpplc.springexamples.rocketship.partha2.Engine;
+import uk.co.tpplc.springexamples.rocketship.partha2.SpaceShuttle;
 
 public class EngineTest {
 
 	@Test
 	public void testArianRocket() {
-	
-	Engine rocketType = new ArianRocket();
-	assertEquals("Arian", rocketType.getType());	
-	
+
+		Engine rocketType = new ArianRocket();
+		assertEquals("Arian", rocketType.getType());
+
 	}
 
 	@Test
 	public void testSpaceShuttle() {
-	
-	Engine rocketType = new SpaceShuttle();
-	assertEquals("Space Shuttle", rocketType.getType());		
+
+		Engine rocketType = new SpaceShuttle();
+		assertEquals("Space Shuttle", rocketType.getType());
 
 	}
-	
 
 }
-
-
--- src/springdemo/test/java/uk/co/tpplc/springexample/rocketship/partha/RocketShipTest.java
+++ src/springdemo/test/java/uk/co/tpplc/springexample/rocketship/partha/RocketShipTest.java
@@ -1,53 +1,55 @@
 package uk.co.tpplc.springexample.rocketship.partha;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
+import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
 
-import uk.co.tpplc.springexamples.container.Car;
+import uk.co.tpplc.springexamples.rocketship.partha2.Engine;
+import uk.co.tpplc.springexamples.rocketship.partha2.Pilot;
+import uk.co.tpplc.springexamples.rocketship.partha2.RocketShip;
+import uk.co.tpplc.springexamples.rocketship.partha2.Weapon;
 
 @ContextConfiguration(locations = { "/uk/co/tpplc/springexamples/RocketShip/Partha/RocketShipTest-context.xml" })
-public class RocketShipTest extends AbstractJUnit4SpringContextTests  {
+public class RocketShipTest extends AbstractJUnit4SpringContextTests {
 
 	@Autowired
 	private RocketShip rocketShip;
 
 	@Autowired
 	private Engine arian;
-	
+
 	@Autowired
 	private Engine spaceshuttle;
-	
+
 	@Autowired
 	private Pilot josh;
-	
+
 	@Autowired
 	private Weapon laser;
-	
+
 	@Test
-	public void makeSureRocketIsWiredUp()
-	{
+	public void makeSureRocketIsWiredUp() {
 		assertEquals("Moon Lander", rocketShip.getType());
 		assertEquals("Arian Rocket", rocketShip.getEngine().getType());
-		assertEquals("Josh reporting for duty!", rocketShip.getPilot().getMessage());
-		assertEquals("Dual-mounted hyper laser beams", rocketShip.getWeapon().getType());
-	}
-	
-	@Test
-	public void arianShouldReturnCorrectType() {
-						
-		assertEquals("Arian Rocket", arian.getType());
-	
+		assertEquals("Josh reporting for duty!", rocketShip.getPilot()
+				.getMessage());
+		assertEquals("Dual-mounted hyper laser beams", rocketShip.getWeapon()
+				.getType());
 	}
 
 	@Test
-	public void spaceShuttleShouldReturnCorrectType()
-	{
+	public void arianShouldReturnCorrectType() {
+
+		assertEquals("Arian Rocket", arian.getType());
+
+	}
+
+	@Test
+	public void spaceShuttleShouldReturnCorrectType() {
 		assertEquals("Space Shuttle", spaceshuttle.getType());
 	}
-	
-	
+
 }
