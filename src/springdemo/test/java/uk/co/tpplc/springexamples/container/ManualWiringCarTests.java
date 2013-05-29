e6a6bcaf16f92563fdfaa7337a53b11d058b5012
chris.pollard <chris.pollard@chaobaohouse.com>
2013-05-29 16:40:04 +0100

    changed springdemo manual wiring test to use completely manual wiring!

--- src/springdemo/test/java/uk/co/tpplc/springexamples/container/ManualWiringCarTests.java
+++ src/springdemo/test/java/uk/co/tpplc/springexamples/container/ManualWiringCarTests.java
@@ -1,41 +1,50 @@
 package uk.co.tpplc.springexamples.container;
 
-import org.apache.tomcat.util.net.SecureNioChannel.ApplicationBufferHandler;
-import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
+import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-@ContextConfiguration(locations = { "/uk/co/tpplc/springexamples/container/ManualCarTests-context.xml" })
-public class ManualWiringCarTests extends AbstractJUnit4SpringContextTests {
+public class ManualWiringCarTests {
 
 	@Autowired
 	private Car car;
-	
-	//based on bean id
+
+	// based on bean id
 	@Autowired
 	private Engine v8;
 
-	//based on bean id
+	// based on bean id
 	@Autowired
 	private Engine diesel;
-	
+
+	@Before
+	public void setup() {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext(
+				"/uk/co/tpplc/springexamples/container/ManualCarTests-context.xml");
+
+		car = (Car) ctx.getBean("car");
+		v8 = (Engine) ctx.getBean("v8");
+		diesel = (Engine) ctx.getBean("diesel");
+	}
+
 	@Test
 	public void seeIfCarIsWiredUp() {
 
 		System.out.println(car.start());
 		assertEquals("chevrolet", car.getMake());
 	}
-	
+
 	@Test
-	public void v8ShouldBeV8(){
+	public void v8ShouldBeV8() {
 		assertEquals("V8", v8.getType());
 	}
 
 	@Test
-	public void flat4ShouldBeFlat4Diesel(){
+	public void flat4ShouldBeFlat4Diesel() {
 		assertEquals("Flat4Diesel", diesel.getType());
 	}
 }
